3585d19d0b3114ecf579c6af1e3c525755cd5fd3
squaq <evertonsquaq@gmail.com>
2016-08-31 23:38:06 -0300

    first

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name multiplusApp
+ * @description
+ * # multiplusApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('multiplusApp', []);
