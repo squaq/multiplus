0bd43fd004a9b15064acdd1c617f9c19a07089c9
TissTech - Danilo S <tisstech@Mac-mini-de-TissTech.local>
2016-09-06 17:58:18 -0300

    another things without css

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,35 @@
  * Controller of the multiplusApp
  */
 angular.module('multiplusApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.form = {};
+    $scope.form.nome = '';
+    $scope.form.email = '';
+    $scope.form.msg = '';
+    $scope.warning = false;
+    
+    $scope.submit = function(){
+        
+        $scope.warning = validForm();
+        if(!$scope.warning){
+//            send message
+            console.log($scope.form, 'time to send message');
+        }
+    }
+    
+    $scope.focus = function(){
+        $scope.warning = false;
+    }
+    
+    function validForm(){
+        if($scope.form.nome.length === 0 || !$scope.form.nome.trim()){
+            return '*preencha o campo nome.'
+        }
+        
+        if($scope.form.email.length === 0 || !/\S+@\S+\.\S+/.test($scope.form.email)){
+            return '*digite um email válido.'
+        }
+        
+        return false;
+    }
   });
