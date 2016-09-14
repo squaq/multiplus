de59163253cececccc856cf7eda5c9277b68d83d
squaq <evertonsquaq@gmail.com>
2016-09-13 23:31:00 -0300

    640 responsive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,19 @@
  * Main module of the application.
  */
 angular
-  .module('multiplusApp', []);
+  .module('multiplusApp', [])
+    .run(function(){
+    angular.element(document).ready(function(){
+        angular.element(".navbar-toggle").on("click", function () {
+                  console.log('clicou')
+				    angular.element(this).toggleClass("active");
+			  });
+    })
+});
+//    angular.document.ready(function () {
+//			  $(".navbar-toggle").on("click", function () {
+//                  console.log('clicou')
+//				    $(this).toggleClass("active");
+//			  });
+//		});
+//};
